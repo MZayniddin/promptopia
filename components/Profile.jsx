faeb33dbf49fc7c326210847cbd57cc90314f53d
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-09-30 18:06:47 +0500

    added user profile

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,8 @@
 import PromptCard from "./PromptCard";
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
-  return (
+  console.log(data)
+    return (
     <section className="w-full">
       <h1 className="head_text text-left">
         <span className="blue_gradient">{name} Profile</span>
